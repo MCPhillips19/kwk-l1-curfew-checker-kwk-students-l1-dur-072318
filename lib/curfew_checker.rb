68c95d6ea781bd7adc80dbd3bb1d28eeebe3b5c1
MCPhillips19 <mpclaire72@gmail.com>
2018-07-26 15:28:45 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -1,9 +1,13 @@
 def simple_curfew_checker(time)
-  # code goes here
+  if  time < 20
+    puts "four plus five equals nine"
+
+   end
 end
 
 def curfew_checker(time)
-  # code goes here
+  if time+ 8= 17
+    puts "fun times"
 end
 
 def complex_curfew_checker(time)
@@ -17,3 +21,4 @@ end
 def platinum_curfew_checker(current_time, curfew_time)
   # code goes here
 end
+simple_curfew_checker(8)
